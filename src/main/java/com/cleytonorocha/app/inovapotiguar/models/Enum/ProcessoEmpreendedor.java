bacea35e30570bf24209271a920cff8bcf7ccb3c
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-16 14:01:57 -0300

    feat: processo empreendedor  entity

---
+++ src/main/java/com/cleytonorocha/app/inovapotiguar/models/Enum/ProcessoEmpreendedor.java
@@ -0,0 +1,30 @@
+package com.cleytonorocha.app.inovapotiguar.models.Enum;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ProcessoEmpreendedor {
+    DESPERTAR_EMPREENDEDOR(0, "Despertar Empreendedor"),
+    PRIMEIRA_IDEIA(1, "Primeira Idéia"),
+    PRIMEIRA_VENDA(2, "Primeira Venda"),
+    ESCALADA_COMERCIAL_TECNOLOGICA(3, "Escala Comercial e Tecnologia"),
+    RENOVACAO(4, "Renovação");
+
+    private final Integer cod;
+    private final String texto;
+
+    public static ProcessoEmpreendedor toEnum(Integer cod) {
+        if (cod == null)
+            return null;
+
+        for (ProcessoEmpreendedor interable : ProcessoEmpreendedor.values()) {
+            if (cod.equals(interable.getCod()))
+                return interable;
+        }
+
+        
+        throw new EnumIdInvalid("Código enum ProcessoEmpreendedor inválido");
+    }
+}
