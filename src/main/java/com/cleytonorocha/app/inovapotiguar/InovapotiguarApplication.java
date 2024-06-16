e5327a188aa861a82ce636e6dce5628c57cad8be
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-16 14:02:36 -0300

    fix: app na nuvem

---
+++ src/main/java/com/cleytonorocha/app/inovapotiguar/InovapotiguarApplication.java
@@ -0,0 +1,13 @@
+package com.cleytonorocha.app.inovapotiguar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovapotiguarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovapotiguarApplication.class, args);
+	}
+
+}
