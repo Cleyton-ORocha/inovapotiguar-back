537a239040812696d6dfe1a5ac8707f83a72395a
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-16 15:21:30 -0300

    feat: Instituicao entity

---
+++ src/main/java/com/cleytonorocha/app/inovapotiguar/models/entity/Instituicao.java
@@ -0,0 +1,32 @@
+package com.cleytonorocha.app.inovapotiguar.models.entity;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Instituicao {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+
+    private String descricao;
+
+    @CNPJ
+    private String CNPJ;
+
+    @Enumerated(EnumType.STRING)
+    private InstituicaoCategoria categoria;
+
+    private String site;
+    private String email;
+    private String linkedin;
+    private String instagram;
+
+}
