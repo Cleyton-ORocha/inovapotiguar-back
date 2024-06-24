f8a07cc7d6f1076fedcdf9e886daa531031b84da
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-23 21:45:45 -0300

    feat: StatusPagamento Enum

---
+++ src/main/java/com/cleytonorocha/app/inovapotiguar/models/Enum/StatusPagamento.java
@@ -0,0 +1,33 @@
+package com.cleytonorocha.app.inovapotiguar.models.Enum;
+
+import com.cleytonorocha.app.inovapotiguar.errors.EnumIdInvalid;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StatusPagamento {
+    PENDENTE(0, "Pendente"),
+    PAGO(1, "Pago"),
+    CANCELADO(2, "Cancelado"),
+    FALHOU(3, "Falhou"),
+    REEMBOLSADO(4, "Reembolsado");
+
+    private final Integer cod;
+    private final String descricao;
+
+    public static StatusPagamento toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+
+        for (StatusPagamento status : StatusPagamento.values()) {
+            if (cod.equals(status.getCod())) {
+                return status;
+            }
+        }
+
+        throw new EnumIdInvalid();
+    }
+}
