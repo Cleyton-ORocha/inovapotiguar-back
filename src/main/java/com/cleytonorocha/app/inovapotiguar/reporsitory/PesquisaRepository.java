8e28b5db03150935d4ce241fa370806d50c8c4e1
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-25 15:32:33 -0300

    feat: Pesquisa Repository

---
+++ src/main/java/com/cleytonorocha/app/inovapotiguar/reporsitory/PesquisaRepository.java
@@ -0,0 +1,9 @@
+package com.cleytonorocha.app.inovapotiguar.reporsitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cleytonorocha.app.inovapotiguar.model.entity.Pesquisa;
+
+public interface PesquisaRepository extends JpaRepository<Pesquisa, Long>{
+    
+}
